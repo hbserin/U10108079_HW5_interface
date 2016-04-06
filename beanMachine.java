5d42633c17e78b283488b870f0bb3e14fc9c0b01
hbserin <hbs_erin@yahoo.com.tw>
2016-04-06 16:34:02 +0800

    Create a pane and a scene

--- beanMachine.java
+++ beanMachine.java
@@ -1,1 +1,25 @@
+/**Name:Erin
+ *Id:U10108079
+ *Exercise:HW5_interface
+ */
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class beanMachine extends Application {
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		// Create a pane and set its properties
+		GridPane pane = new GridPane();
+
+		
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane);
+		primaryStage.setTitle("Bean Machine Interface"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+}

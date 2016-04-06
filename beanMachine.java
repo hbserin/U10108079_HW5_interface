4c1befc1b20adbc0623e6df9ec7c19d267cfba52
hbserin <hbs_erin@yahoo.com.tw>
2016-04-06 17:39:59 +0800

    create some inside lines

--- beanMachine.java
+++ beanMachine.java
@@ -16,10 +16,17 @@ public class beanMachine extends Application {
 		// Create a pane
 		Pane pane = new Pane();
 
-		 //create an outline
+		//create an outline
 		Polyline polyline = new Polyline();
 		polyline.getPoints().addAll(200.0,50.0, 200.0,100.0, 100.0,300.0, 100.0,400.0, 400.0,400.0, 400.0,300.0, 300.0,100.0, 300.0,50.0);
 		pane.getChildren().add(polyline);
+
+		//create some inside lines
+		Line[] line = new Line[7];
+		for(int i=0;i<7;i++){
+			line[i] = new Line(100+i*(300/7),300, 100+i*(300/7),400);
+			pane.getChildren().add(line[i]);	
+		}
 
 		// Create a scene and place it in the stage
 		Scene scene = new Scene(pane,500,450); // create a scene for the pane

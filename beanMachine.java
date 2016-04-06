9e0db143e1b879e48e979c8e2ac545f7d1922b34
hbserin <hbs_erin@yahoo.com.tw>
2016-04-06 17:29:55 +0800

    set the scene size and create the outline

--- beanMachine.java
+++ beanMachine.java
@@ -5,19 +5,24 @@
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
 
 
 public class beanMachine extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
-		// Create a pane and set its properties
-		GridPane pane = new GridPane();
+		// Create a pane
+		Pane pane = new Pane();
 
-		
+		 //create an outline
+		Polyline polyline = new Polyline();
+		polyline.getPoints().addAll(200.0,50.0, 200.0,100.0, 100.0,300.0, 100.0,400.0, 400.0,400.0, 400.0,300.0, 300.0,100.0, 300.0,50.0);
+		pane.getChildren().add(polyline);
+
 		// Create a scene and place it in the stage
-		Scene scene = new Scene(pane);
+		Scene scene = new Scene(pane,500,450); // create a scene for the pane
 		primaryStage.setTitle("Bean Machine Interface"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
